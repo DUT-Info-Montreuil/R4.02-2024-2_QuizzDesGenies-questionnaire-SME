8b1a73b0eb47747ce63d5b97d4c668421408feb7
XiangLuc <xiangluc01@gmail.com>
2024-03-13 15:23:58 +0100

    Ajout de la conversion pour le Integer

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/entities/mapper/QuestionBoVersQuestionDTOMapper.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/entities/mapper/QuestionBoVersQuestionDTOMapper.java
@@ -7,12 +7,12 @@ import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.utiles.enums
 public class QuestionBoVersQuestionDTOMapper {
 
     public QuestionDTO convetir(QuestionBO questionBO) {
-        int num_question = Integer.parseInt(questionBO.getNum_question());
-        int id_question = Integer.parseInt(questionBO.getId_question());
+        int num_question = Integer.parseInt(questionBO.getNum_question().replaceAll("[^\\x00-\\x7F]", ""));
+        int id_question = Integer.parseInt(questionBO.getId_question().replaceAll("[^\\x00-\\x7F]", ""));
         Langue langue = Langue.valueOf(questionBO.getLangue());
         String libelle_question = questionBO.getLibelle_question();
         String explication = questionBO.getExplication();
-        int difficulte = Integer.parseInt(questionBO.getDifficulte());
+        int difficulte = Integer.parseInt(questionBO.getDifficulte().replaceAll("[^\\x00-\\x7F]", ""));
         return new QuestionDTO(num_question,id_question,langue,libelle_question,explication,difficulte);
     }
 }
