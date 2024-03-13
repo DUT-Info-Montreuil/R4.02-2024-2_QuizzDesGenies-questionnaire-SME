df74a638b470f5e486bf6d314b70660ddb580bad
XiangLuc <xiangluc01@gmail.com>
2024-03-13 12:42:50 +0100

    Interface des questionnaires avec la fonction de génération de questionnaire

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/services/interfaces/IQuestionnaire.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/services/interfaces/IQuestionnaire.java
@@ -1,17 +1,11 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.services.interfaces;
 
-import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.dto.QuestionnaireDTO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.bo.QuestionBO;
 
 import java.util.ArrayList;
 
 public interface IQuestionnaire {
 
-    public ArrayList<QuestionnaireDTO> fournirListeQuestionnaires();
-
-    public QuestionnaireDTO fournirUnQuestionnaire(int numQuestionnaire);
-
-    public void majStatQuestions();
-
-    public void fournirStatsQuestions();
+    public ArrayList<QuestionBO> chargerQuestionnaireCSV(String path);
 
 }
