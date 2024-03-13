d06c96c01b1277d2f2e5cba989c4d6be95b5a51b
XiangLuc <xiangluc01@gmail.com>
2024-03-13 12:42:17 +0100

    QuestionDTO avec un id, num question, une enumération des langues, un libellé,reponse et difficulte

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/entities/dto/QuestionDTO.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/entities/dto/QuestionDTO.java
@@ -1,26 +1,15 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.dto;
 
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.utils.Langue;
+
 public class QuestionDTO {
 
     private int id_questionnaire;
-    private int num_question;
-    private String langue;
+    private String num_question;
+    private Enum<Langue> langue;
     private String libelle_question;
     private String reponse;
     private int difficulte;
-    private String explication;
-    private String reference;
-
-    public QuestionDTO(int id_questionnaire, int num_question, String langue, String libelle_question, String reponse, int difficulte, String explication, String reference) {
-        this.id_questionnaire = id_questionnaire;
-        this.num_question = num_question;
-        this.langue = langue;
-        this.libelle_question = libelle_question;
-        this.reponse = reponse;
-        this.difficulte = difficulte;
-        this.explication = explication;
-        this.reference = reference;
-    }
 
     public int getId_questionnaire() {
         return id_questionnaire;
@@ -30,19 +19,19 @@ public class QuestionDTO {
         this.id_questionnaire = id_questionnaire;
     }
 
-    public int getNum_question() {
+    public String getNum_question() {
         return num_question;
     }
 
-    public void setNum_question(int num_question) {
+    public void setNum_question(String num_question) {
         this.num_question = num_question;
     }
 
-    public String getLangue() {
+    public Enum<Langue> getLangue() {
         return langue;
     }
 
-    public void setLangue(String langue) {
+    public void setLangue(Enum<Langue> langue) {
         this.langue = langue;
     }
 
@@ -68,35 +57,5 @@ public class QuestionDTO {
 
     public void setDifficulte(int difficulte) {
         this.difficulte = difficulte;
-    }
-
-    public String getExplication() {
-        return explication;
-    }
-
-    public void setExplication(String explication) {
-        this.explication = explication;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public void setReference(String reference) {
-        this.reference = reference;
-    }
-
-    @Override
-    public String toString() {
-        return "QuestionDTO{" +
-                "id_questionnaire=" + id_questionnaire +
-                ", num_question=" + num_question +
-                ", langue='" + langue + '\'' +
-                ", libelle_question='" + libelle_question + '\'' +
-                ", reponse='" + reponse + '\'' +
-                ", difficulte=" + difficulte +
-                ", explication='" + explication + '\'' +
-                ", reference='" + reference + '\'' +
-                '}';
     }
 }
