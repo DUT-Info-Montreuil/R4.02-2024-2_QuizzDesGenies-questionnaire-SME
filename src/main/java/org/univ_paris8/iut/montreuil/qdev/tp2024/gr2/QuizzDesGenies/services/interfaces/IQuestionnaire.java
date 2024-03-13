4d76e394455015d9511d7bf7bf29a1912472b327
XiangLuc <xiangluc01@gmail.com>
2024-03-13 15:23:32 +0100

    Correction de la signature de la méthode

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/services/interfaces/IQuestionnaire.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/services/interfaces/IQuestionnaire.java
@@ -1,12 +1,13 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.services.interfaces;
 
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.bo.QuestionBO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.dto.QuestionnaireDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.utiles.exceptions.FichierNonTrouverException;
 
 import java.util.ArrayList;
 
 public interface IQuestionnaire {
 
-    public ArrayList<QuestionBO> chargerQuestionnaireCSV(String path) throws FichierNonTrouverException;
+    public QuestionnaireDTO chargerQuestionnaireCSV(String path) throws FichierNonTrouverException;
 
 }
