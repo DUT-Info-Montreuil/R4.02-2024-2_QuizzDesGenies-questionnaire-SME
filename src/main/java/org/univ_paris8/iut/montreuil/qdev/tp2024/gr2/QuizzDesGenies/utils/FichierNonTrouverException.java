6f94e5820f0f85ea8f1f3ab6767ed8ef8b1a3bbb
XiangLuc <xiangluc01@gmail.com>
2024-03-13 12:54:40 +0100

    changement sur la localisation des fichiers sur le dossiers utils

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/utils/FichierNonTrouverException.java
+++
@@ -1,5 +0,0 @@
-package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.utils;
-
-public class FichierNonTrouverException extends Exception{
-
-}
---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/utils/exceptions/FichierNonTrouverException.java
@@ -0,0 +1,5 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.utils.exceptions;
+
+public class FichierNonTrouverException extends Exception{
+
+}
