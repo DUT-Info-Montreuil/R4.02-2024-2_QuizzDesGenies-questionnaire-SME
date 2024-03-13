720aa1d0e12308b5b325cbe43f0de7c9be95f6b6
XiangLuc <xiangluc01@gmail.com>
2024-03-13 12:59:14 +0100

    création QuestionnairesDTO

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/entities/dto/QuestionnairesDTO.java
@@ -0,0 +1,20 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.dto;
+
+import java.util.ArrayList;
+
+public class QuestionnairesDTO {
+
+    private ArrayList<QuestionnaireDTO> liste_questionnaire;
+
+    public QuestionnairesDTO(ArrayList<QuestionnaireDTO> liste_questionnaire) {
+        this.liste_questionnaire = liste_questionnaire;
+    }
+
+    public ArrayList<QuestionnaireDTO> getListe_questionnaire() {
+        return liste_questionnaire;
+    }
+
+    public void setListe_questionnaire(ArrayList<QuestionnaireDTO> liste_questionnaire) {
+        this.liste_questionnaire = liste_questionnaire;
+    }
+}
