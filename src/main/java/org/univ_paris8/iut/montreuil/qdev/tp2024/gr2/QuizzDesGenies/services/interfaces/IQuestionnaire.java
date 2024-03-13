bb6aa1c6632c6e06c31dadc42fc7cdd7620fd6b6
XiangLuc <xiangluc01@gmail.com>
2024-03-13 14:03:16 +0100

    ajout du throw

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/services/interfaces/IQuestionnaire.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/services/interfaces/IQuestionnaire.java
@@ -1,11 +1,12 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.services.interfaces;
 
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.bo.QuestionBO;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.utiles.exceptions.FichierNonTrouverException;
 
 import java.util.ArrayList;
 
 public interface IQuestionnaire {
 
-    public ArrayList<QuestionBO> chargerQuestionnaireCSV(String path);
+    public ArrayList<QuestionBO> chargerQuestionnaireCSV(String path) throws FichierNonTrouverException;
 
 }
