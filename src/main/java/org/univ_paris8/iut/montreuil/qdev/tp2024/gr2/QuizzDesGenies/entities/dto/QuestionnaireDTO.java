f1ccf85c2baf0232dbd5c32b75a1e3963144b857
XiangLuc <xiangluc01@gmail.com>
2024-03-13 12:40:28 +0100

    QuestionnaireDTO pour récupérer une liste de QuestionDTO

--- src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/entities/dto/QuestionnaireDTO.java
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/entities/dto/QuestionnaireDTO.java
@@ -1,30 +1,13 @@
 package org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.dto;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 public class QuestionnaireDTO {
 
-    private String nom_questionnaire;
     private ArrayList<QuestionDTO> questionnaire;
 
-    public QuestionnaireDTO(String nom_questionnaire, ArrayList<QuestionDTO> questionnaire) {
-        this.nom_questionnaire = nom_questionnaire;
+    public QuestionnaireDTO(ArrayList<QuestionDTO> questionnaire) {
         this.questionnaire = questionnaire;
-    }
-
-    public QuestionnaireDTO(String nom_questionnaire) {
-        this.nom_questionnaire = nom_questionnaire;
-        this.questionnaire = new ArrayList<>();
-    }
-
-    public String getNom_questionnaire() {
-        return nom_questionnaire;
-    }
-
-    public void setNom_questionnaire(String nom_questionnaire) {
-        this.nom_questionnaire = nom_questionnaire;
     }
 
     public ArrayList<QuestionDTO> getQuestionnaire() {
@@ -33,5 +16,13 @@ public class QuestionnaireDTO {
 
     public void setQuestionnaire(ArrayList<QuestionDTO> questionnaire) {
         this.questionnaire = questionnaire;
+    }
+
+    public void addQuestion(QuestionDTO question) {
+        questionnaire.add(question);
+    }
+
+    public void deleteQuestion(QuestionDTO question) {
+        questionnaire.remove(question);
     }
 }
