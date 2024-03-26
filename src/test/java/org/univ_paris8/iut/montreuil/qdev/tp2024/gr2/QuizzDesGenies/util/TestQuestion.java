41153ffce37ee5f0a3377d4f492fef7400f5bbbc
XiangLuc <xiangluc01@gmail.com>
2024-03-26 15:54:27 +0100

    merge du test

--- src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/util/TestQuestion.java
+++ src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/util/TestQuestion.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.dto.QuestionDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.entities.dto.QuestionnaireDTO;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.service.impl.QuestionnaireImplMock;
+import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.services.impl.QuestionnaireImpl;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.services.interfaces.IQuestionnaire;
 import org.univ_paris8.iut.montreuil.qdev.tp2024.gr2.QuizzDesGenies.utiles.exceptions.FichierNonTrouverException;
 
@@ -18,9 +19,8 @@ public class TestQuestion {
     @Test
     public void testChargerQuestionnaireCSV() throws FichierNonTrouverException {
         // Initialisation
-        IQuestionnaire questionnaire = new QuestionnaireImplMock();
-        String chemin = "/home/etudiants/info/angatchou/Bureau/Semetre3/Dev Efficace/R4.02-2024-2_QuizzDesGenies-questionnaire-SME/src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/ressources/questionsQuizz_V1.1.csv";
-
+        IQuestionnaire questionnaire = new QuestionnaireImpl();
+        String chemin = "src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/ressources/questionsQuizz_V1.1.csv";
         // Exécution
         QuestionnaireDTO questions = questionnaire.chargerQuestionnaireCSV(chemin);
 
@@ -42,7 +42,7 @@ public class TestQuestion {
     public void testChargerQuestionnaireCSVFichierNonTrouve() throws FichierNonTrouverException {
         //IQuestionnaire questionnaire = new QuestionnaireImplMock();
         //String chemin = "src/test/java/org/univ_paris8/iut/montreuil/qdev/tp2024/gr2/QuizzDesGenies/service/impl/QuestionnaireImplMock.java";
-          String chemin = " ";
+        String chemin = "";
 
 
         File file = new File(chemin);
